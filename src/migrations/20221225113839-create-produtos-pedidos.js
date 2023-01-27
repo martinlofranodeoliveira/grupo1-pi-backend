d0e424cb30073fa11fed0c5b797a951631d159c7
Demetrius Macêdo <macedo.demetrius@gmail.com>
2023-01-26 21:11:49 -0300

    Update 20221225113839-create-produtos-pedidos.js

--- src/migrations/20221225113839-create-produtos-pedidos.js
+++ src/migrations/20221225113839-create-produtos-pedidos.js
@@ -35,3 +35,60 @@ module.exports = {
     await queryInterface.dropTable('ProdutosPedidos');
   }
 };
+
+// 'use strict';
+
+// const {
+// INTEGER,
+// DATE,
+// STRING,
+// } = require('sequelize');
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+// async up(queryInterface, Sequelize) {
+// await queryInterface.createTable('ProdutosPedidos', {
+// id: {
+// allowNull: false,
+// autoIncrement: true,
+// primaryKey: true,
+// type: INTEGER
+// },
+// produtos_id: {
+// allowNull: false,
+// type: INTEGER,
+// references: {
+// model: 'Produtos',
+// key: 'id'
+// },
+// onUpdate: 'CASCADE',
+// onDelete: 'CASCADE'
+// },
+// pedidos_id: {
+// allowNull: false,
+// type: INTEGER,
+// references: {
+// model: 'Pedidos',
+// key: 'id'
+// },
+// onUpdate: 'CASCADE',
+// onDelete: 'CASCADE'
+// },
+// quantidade: {
+// allowNull: false,
+// type: INTEGER
+// },
+// createdAt: {
+// allowNull: false,
+// type: DATE
+// },
+// updatedAt: {
+// allowNull: false,
+// type: DATE
+// }
+// });
+// },
+// async down(queryInterface, Sequelize) {
+// await queryInterface.dropTable('ProdutosPedidos');
+// }
+// };
